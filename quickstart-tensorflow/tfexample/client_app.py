be81d2ed25997dd5c9acfcecc9ca4b49d147f5fa
Matej Vesel <matej.vesel@result.si>
2026-05-20 09:09:03 +0200

    chore: In the middle of replacing quickstart model with apnea predictions model.

--- quickstart-tensorflow/tfexample/client_app.py
+++ quickstart-tensorflow/tfexample/client_app.py
@@ -4,7 +4,8 @@ import keras
 from flwr.app import ArrayRecord, Context, Message, MetricRecord, RecordDict
 from flwr.clientapp import ClientApp
 
-from tfexample.task import load_data, load_model
+from tfexample.config.config_rok_factor_1_brez_C_v4_0 import config
+from tfexample.task_apnea import train_model
 
 # Flower ClientApp
 app = ClientApp()
@@ -17,68 +18,52 @@ def train(msg: Message, context: Context):
     # Reset local Tensorflow state
     keras.backend.clear_session()
 
-    # Load the data
-    partition_id = context.node_config["partition-id"]
-    num_partitions = context.node_config["num-partitions"]
-    x_train, y_train, _, _ = load_data(partition_id, num_partitions)
-
-    # Load the model
-    model = load_model(context.run_config["learning-rate"])
-    model.set_weights(msg.content["arrays"].to_numpy_ndarrays())
-    epochs = context.run_config["local-epochs"]
-    batch_size = context.run_config["batch-size"]
-    verbose = context.run_config.get("verbose")
-
     # Train the model
-    history = model.fit(
-        x_train,
-        y_train,
-        epochs=epochs,
-        batch_size=batch_size,
-        verbose=verbose,
-    )
+    settings = config["settings"]
+    general_parameters = config["general_parameters"]
+    hyper_parameters = config["hyper_parameters"]
+    metrics, model = train_model(settings, general_parameters, hyper_parameters)
 
-    # Get training metrics
-    train_loss = history.history["loss"][-1] if "loss" in history.history else None
-    train_acc = (
-        history.history["accuracy"][-1] if "accuracy" in history.history else None
-    )
 
     # Pack and send the model weights and metrics as a message
-    model_record = ArrayRecord(model.get_weights())
-    metrics = {"num-examples": len(x_train)}
-    if train_loss is not None:
-        metrics["train_loss"] = train_loss
-    if train_acc is not None:
-        metrics["train_acc"] = train_acc
-    content = RecordDict({"arrays": model_record, "metrics": MetricRecord(metrics)})
+    content = RecordDict({"arrays": ArrayRecord(model.get_weights()), "metrics": MetricRecord(metrics)})
     return Message(content=content, reply_to=msg)
 
 
-@app.evaluate()
-def evaluate(msg: Message, context: Context):
-    """Evaluate the model on local data."""
-
-    # Reset local Tensorflow state
-    keras.backend.clear_session()
-
-    # Load the data
-    partition_id = context.node_config["partition-id"]
-    num_partitions = context.node_config["num-partitions"]
-    _, _, x_test, y_test = load_data(partition_id, num_partitions)
-
-    # Load the model
-    model = load_model(context.run_config["learning-rate"])
-    model.set_weights(msg.content["arrays"].to_numpy_ndarrays())
-
-    # Evaluate the model
-    eval_loss, eval_acc = model.evaluate(x_test, y_test, verbose=0)
-
-    # Pack and send the model weights and metrics as a message
-    metrics = {
-        "eval_acc": eval_acc,
-        "eval_loss": eval_loss,
-        "num-examples": len(x_test),
-    }
-    content = RecordDict({"metrics": MetricRecord(metrics)})
-    return Message(content=content, reply_to=msg)
+# @app.evaluate()
+# def evaluate(msg: Message, context: Context):
+#     """Evaluate the model on local data."""
+#
+#     # Reset local Tensorflow state
+#     keras.backend.clear_session()
+#
+#     # Train the model
+#     settings = config["settings"]
+#     general_parameters = config["general_parameters"]
+#     hyper_parameters = config["hyper_parameters"]
+#     metrics, model = train_model(settings, general_parameters, hyper_parameters)
+#
+#     # Pack and send the model weights and metrics as a message
+#     content = RecordDict({"arrays": ArrayRecord(model.get_weights()), "metrics": MetricRecord(metrics)})
+#     return Message(content=content, reply_to=msg)
+#
+#     # Load the data
+#     partition_id = context.node_config["partition-id"]
+#     num_partitions = context.node_config["num-partitions"]
+#     _, _, x_test, y_test = load_data(partition_id, num_partitions)
+#
+#     # Load the model
+#     model = load_model(context.run_config["learning-rate"])
+#     model.set_weights(msg.content["arrays"].to_numpy_ndarrays())
+#
+#     # Evaluate the model
+#     eval_loss, eval_acc = model.evaluate(x_test, y_test, verbose=0)
+#
+#     # Pack and send the model weights and metrics as a message
+#     metrics = {
+#         "eval_acc": eval_acc,
+#         "eval_loss": eval_loss,
+#         "num-examples": len(x_test),
+#     }
+#     content = RecordDict({"metrics": MetricRecord(metrics)})
+#     return Message(content=content, reply_to=msg)
---
+++ quickstart-tensorflow/tfexample/config/config_rok_factor_1_brez_C_v4_0.py
@@ -0,0 +1,62 @@
+factor = 1
+
+settings = {
+    "config_name": "config_strict_good",
+    "experiment_name": "Hyperparameter Tuning Omop 1s",
+    "study_name": f"Objective: Val F1 Score (Variance to Rok: {factor}) Brez C10 (1) in C28 (20)",
+    "output_path": "./output", # Relative to tuning.py.
+    "input_path": "./input", # Relative to tuning.py.
+    "dataset_filename": "ml_train_dataset_from_omop_1s.csv",
+    "log_file": "log.txt", # Do not change this.
+    "objective_metric": "true_val_f1_score", # For possible options, see final_metrics variable in train_model.py.
+    "objective_direction": "maximize", # Either 'maximize' or 'minimize'
+    "epochs": 2,
+    "trials": 2
+}
+
+general_parameters = {
+    "FEATURES": [ "value_as_number_spo2", "value_as_number_hr" ],
+    "TARGET": "value_as_number_apnea",
+    "ID_COLUMN": "person_id",
+    "TEST_PERSON_IDS": [1, 20],
+    "WINDOW": 75, # Window size: 5 minutes (4s x 75 = 300s = 5 min)
+    "STEP": 15 # Step size: size of a step to take on moving Window
+}
+
+def make_positive(x, bo):
+    if x <= 0:
+        return 1 * bo
+    else:
+        return x
+
+v1 = 5 * factor
+v2 = 0.2 * (factor * 0.1)
+v3 = 0.005 * (factor * 0.001)
+v4 = 0.00005 * (factor * 0.00001)
+hyper_parameters = {
+    "window_size": [make_positive(75 - v1, 1), make_positive(75 + v1, 1)],
+    "lstm_units": [
+        [make_positive(64-v1, 1), make_positive(64+v1, 1)], # Lower
+        [make_positive(32-v1, 1), make_positive(32+v1, 1)],
+        [make_positive(16-v1, 1), make_positive(16+v1, 1)], # Higher
+    ],
+    "depth": [make_positive(6-v1, 1), make_positive(6+v1, 1)],
+    "nb_filters": [make_positive(32-v1, 1), make_positive(32+v1, 1)], # Higher
+    "dropout_dense": [
+        [make_positive(0.5-v2, 0.1), make_positive(0.5+v2, 0.1)],
+        [make_positive(0.4-v2, 0.1), make_positive(0.4+v2, 0.1)],
+    ],
+    "dropout_lstm": [make_positive(0.4-v2, 0.1), make_positive(0.4+v2, 0.1)],
+    "l2_reg": [make_positive(0.01-v3, 0.001), make_positive(0.01+v3, 0.001)], # It was log!
+    "learning_rate": [make_positive(0.0005-v4, 0.00001), make_positive(0.0005+v4, 0.00001)], # It was log!
+    "epochs": settings["epochs"],
+    "batch_size": [make_positive(128-v1, 1), make_positive(128+v1, 1)], # Lower
+    "prediction_threshold": [make_positive(0.5-v2, 0.1), make_positive(0.5+v2, 0.1)],
+    "validation_split": [make_positive(0.2-v2, 0.1), make_positive(0.2+v2, 0.1)] # Lower
+}
+
+config = {
+    "settings": settings,
+    "general_parameters": general_parameters,
+    "hyper_parameters": hyper_parameters
+}
