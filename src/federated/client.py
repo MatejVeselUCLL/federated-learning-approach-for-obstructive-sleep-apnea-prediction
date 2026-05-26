a766470a12441c201e382decf9e2f136880379a9
Matej Vesel <matej.vesel@result.si>
2026-05-26 15:18:01 +0200

    feat: Evaluation improvements

--- src/federated/client.py
+++ src/federated/client.py
@@ -46,8 +46,5 @@ def evaluate(msg: Message, context: Context):
     keras.backend.clear_session()
 
     metrics = evaluate_model(msg, context)
-    print("METRICS")
-    pprint(metrics)
-
     content = RecordDict({"metrics": MetricRecord(metrics)})
     return Message(content=content, reply_to=msg)
