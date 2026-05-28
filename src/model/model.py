a42d0363dc0c338b8aca9e0cef075e164fc0726a
Matej Vesel <matej.vesel@result.si>
2026-05-28 08:27:23 +0200

    chore: training on cpu to avoid OOM error.

--- src/model/model.py
+++ src/model/model.py
@@ -365,6 +365,9 @@ def train_model():
     # In[4]:
 
 
+    # Set the device to CPU.
+    tf.config.set_visible_devices([], 'GPU')
+
     # 1. Identify Physical Devices
     gpus = tf.config.list_physical_devices('GPU')
     cpus = tf.config.list_physical_devices('CPU')
