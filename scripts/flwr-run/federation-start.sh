be477655019df3cc96902cd74f72926a027fc683
Matej Vesel <matej.vesel@result.si>
2026-06-02 12:03:23 +0200

    chore: preparing for comparison

--- scripts/flwr-run/federation-start.sh
+++ scripts/flwr-run/federation-start.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-echo "FEDERATED LEARNING (Obstructive Apnea Predictions)" && echo ""
+echo "FEDERATED LEARNING (Obstructive Apnea Predictions)"
 
 # Variables
 rp="/home/admine/matej/federated-learning-poc" # repository path
 lp="${rp}/src/federated/logs" # logs path
 started_processes=""
-echo "Logs can be found at ${lp}" && echo ""
+echo "Logs can be found at ${lp}"
 
 
 # Virtual environment
-echo "Setting up virtual environment" && echo ""
+echo "Setting up virtual environment"
 cd ${rp} # !important
 source .venv/bin/activate
 
 # Updates
-echo "Setting up git (fetching updates)" && echo ""
-git stash && git pull &> ${lp}/git.log
+echo "Setting up git (fetching updates)"
+{git stash && git pull} &> ${lp}/git.log
 
 # Federated learning
 
 ## Preparation
 
 ### Superlink
-echo "Setting up flower superlink" && echo ""
+echo "Setting up flower superlink"
 flower-superlink --insecure &> ${lp}/super_link.log &
 started_processes="${!}"
 
 ### Supernodes
-echo "Setting up flower supernodes" && echo ""
+echo "Setting up flower supernodes"
 n=1
 while [ ${n} -lt 4 ] # 3 hospitals
 do
     port=9093+${n}
     dataset_filename="h${n}_202605311423.csv"
 
-    echo "    Setting up hospital ${n}" && echo ""
+    echo "    Setting up hospital ${n}"
     flower-supernode \
       --insecure \
       --superlink 127.0.0.1:9092 \
@@ -48,7 +48,7 @@ do
 done
 
 ## Start (run) the process.
-echo "Setting up flower run (starting the learning process)" && echo ""
+echo "Setting up flower run (starting the learning process)"
 flwr run . local-deployment --stream &> ${lp}/run.log &
 started_processes="${started_processes} ${!}"
 
