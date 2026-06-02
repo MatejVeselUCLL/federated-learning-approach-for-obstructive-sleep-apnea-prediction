519a5d71bc9971d0c5b97d2fd98a664e10438495
Matej Vesel <matej.vesel@result.si>
2026-06-02 12:01:32 +0200

    chore: preparing for comparison

--- scripts/flwr-run/federation-start.sh
+++ scripts/flwr-run/federation-start.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
 
+echo "FEDERATED LEARNING (Obstructive Apnea Predictions)" && echo ""
+
 # Variables
 rp="/home/admine/matej/federated-learning-poc" # repository path
 lp="${rp}/src/federated/logs" # logs path
 started_processes=""
+echo "Logs can be found at ${lp}" && echo ""
+
 
 # Virtual environment
+echo "Setting up virtual environment" && echo ""
 cd ${rp} # !important
 source .venv/bin/activate
+
 # Updates
-git pull
+echo "Setting up git (fetching updates)" && echo ""
+git stash && git pull &> ${lp}/git.log
 
 # Federated learning
 
 ## Preparation
 
 ### Superlink
-flower-superlink --insecure &> ${lp}/super_link_log.txt &
+echo "Setting up flower superlink" && echo ""
+flower-superlink --insecure &> ${lp}/super_link.log &
 started_processes="${!}"
 
 ### Supernodes
+echo "Setting up flower supernodes" && echo ""
 n=1
 while [ ${n} -lt 4 ] # 3 hospitals
 do
-    echo "Hospital ${n}"
     port=9093+${n}
     dataset_filename="h${n}_202605311423.csv"
 
+    echo "    Setting up hospital ${n}" && echo ""
     flower-supernode \
       --insecure \
       --superlink 127.0.0.1:9092 \
       --clientappio-api-address 127.0.0.1:${port} \
-      --node-config "dataset-filename=${dataset-filename}" &> ${lp}/h${n}_log.txt &
+      --node-config "dataset-filename=${dataset-filename}" &> ${lp}/h${n}.log &
 
-    started_processes="${started_processes},${!}"
+    started_processes="${started_processes} ${!}"
 
     n=`expr $n + 1`
 done
 
 ## Start (run) the process.
-flwr run . local-deployment --stream &> ${lp}/run_log.txt &
-started_processes="${started_processes},${!}"
+echo "Setting up flower run (starting the learning process)" && echo ""
+flwr run . local-deployment --stream &> ${lp}/run.log &
+started_processes="${started_processes} ${!}"
 
-echo $started_processes
+# Save process ids.
+echo "Started processes: ${started_processes}"
+echo $started_processes &> ${lp}/processes.log
+echo "Kill processes with command: kill ${started_processes}"
+
 
 # Manually running the federation.
 ## Hospital 1
-#flower-supernode --insecure --superlink 127.0.0.1:9092 --clientappio-api-address 127.0.0.1:9094 --node-config 'dataset-filename="h1_202605311423.csv"' &> $lp/h1_log.txt &
+#flower-supernode --insecure --superlink 127.0.0.1:9092 --clientappio-api-address 127.0.0.1:9094 --node-config 'dataset-filename="h1_202605311423.csv"' &> $lp/h1.log &
 ## Hospital 2
-#flower-supernode --insecure --superlink 127.0.0.1:9092 --clientappio-api-address 127.0.0.1:9095 --node-config 'dataset-filename="h2_202605311423.csv"' &> $lp/h2_log.txt &
+#flower-supernode --insecure --superlink 127.0.0.1:9092 --clientappio-api-address 127.0.0.1:9095 --node-config 'dataset-filename="h2_202605311423.csv"' &> $lp/h2.log &
 ## Hospital 3
-#flower-supernode --insecure --superlink 127.0.0.1:9092 --clientappio-api-address 127.0.0.1:9096 --node-config 'dataset-filename="h3_202605311423.csv"' &> $lp/h3_log.txt &
+#flower-supernode --insecure --superlink 127.0.0.1:9092 --clientappio-api-address 127.0.0.1:9096 --node-config 'dataset-filename="h3_202605311423.csv"' &> $lp/h3.log &
