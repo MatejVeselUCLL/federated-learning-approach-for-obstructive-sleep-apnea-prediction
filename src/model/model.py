d68c5c1563cc9ac4d1f65459bc8b13b756aaff25
Matej Vesel <matej.vesel@result.si>
2026-06-06 10:51:40 +0200

    chore: result 2.

--- src/model/model.py
+++ src/model/model.py
@@ -317,7 +317,7 @@ def train_model(dataset_filename=""):
     OUTPUT_PATH = settings["output_path"]
 
     # Take the dataset from the client context
-    print("DDD", dataset_filename)
+    print("Dataset: ", dataset_filename)
     DATASET_FILENAME = dataset_filename if dataset_filename != "" else DATASET_FILENAME
 
     TEST_PERSON_IDS = get_test_person_ids(DATASET_FILENAME)
